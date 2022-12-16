419f290b95212c90940f1b1faa70a109f1299ea7
Amir Kazemi <kazemi.amir68@gmail.com>
2022-12-16 04:25:05 +0100

    add correct usage of component in app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import InfiniteLoader from "./component/InfiniteLoader";
+
 function App() {
-  return <div></div>;
+  const addressGenerator = (page: number): string => {
+    return `https://api.punkapi.com/v2/beers?page=${page}`;
+  };
+
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <InfiniteLoader
+        addressGenerator={addressGenerator}
+        style={{ backgroundColor: "red" }}
+        width={400}
+        height={300}
+        itemSize={40}
+        // itemCount={25}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/index.ts
@@ -0,0 +1,1 @@
+export { default as infiniteLoader } from "./InfiniteLoader";
