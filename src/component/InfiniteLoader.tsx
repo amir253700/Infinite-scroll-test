8240819a20c344096f97531394a7b5b8437bb7ea
Amir Kazemi <kazemi.amir68@gmail.com>
2022-12-16 04:24:30 +0100

    correct component props type

--- src/component/InfiniteLoader.tsx
+++ src/component/InfiniteLoader.tsx
@@ -1,24 +1,95 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
+import {
+  FixedSizeList as List,
+  FixedSizeListProps,
+  ListOnScrollProps,
+} from "react-window";
+
 import useLazyFetch from "../hooks/useLazyFetch";
 
-interface Props {
-  children: React.ReactNode;
-  callback: (pageNumber: number) => string;
+interface Props
+  extends Omit<FixedSizeListProps, "itemCount" | "children" | "onScroll"> {
+  renderFunction?: ({ data, index, style }: ItemProps) => JSX.Element;
+  addressGenerator: (pageNumber: number) => string;
+  loadingElement?: React.ReactNode;
+  errorElement?: React.ReactNode;
 }
 
-const InfiniteLoader = ({ children, callback }: Props): React.ReactNode => {
-  const [url, setUrl] = useState<string | null>(null);
-  const [data, setData] = useState<any>(null);
-  const pageNumber = useRef<number>(0);
-  const [fetchData, { isLoading, result, isError }] = useLazyFetch();
+interface ItemProps {
+  data: any[];
+  index: any;
+  style: any;
+}
 
-  const urlToFetch = callback(pageNumber.current);
+const listItem = ({ data, index, style }: ItemProps): JSX.Element => {
+  return (
+    <div style={style}>
+      {data[index].id} {data[index].name} {index}
+    </div>
+  );
+};
 
-  // useEffect(() => {
-  //   fetchData(urlToFetch);
-  // }, []);
+const InfiniteLoader = ({
+  renderFunction,
+  addressGenerator,
+  loadingElement = <p>Loading...</p>,
+  errorElement = <p>Something went wrong!</p>,
+  ...rest
+}: Props): JSX.Element => {
+  const pageNumber = useRef<number>(1);
+  const isLocked = useRef<boolean>(false);
+  const lastScrollOffset = useRef<number>(0);
+  const listRef = useRef<any>(null);
 
-  return <div></div>;
+  const [fetchData, { isLoading, result, isError }] = useLazyFetch(
+    addressGenerator(1)
+  );
+
+  const loadMoreData = async () => {
+    if (!isLoading && !isLocked.current) {
+      isLocked.current = true;
+      pageNumber.current++;
+      await fetchData(addressGenerator(pageNumber.current));
+      isLocked.current = false;
+    }
+  };
+
+  const scrollHandler = ({ scrollOffset }: ListOnScrollProps): any => {
+    const direction = rest.direction;
+    const listElement = listRef.current;
+    if (listElement) {
+      const listElementHeight =
+        direction === "horizontal"
+          ? listElement.scrollWidth
+          : listElement.scrollHeight;
+      const total =
+        scrollOffset +
+        (direction === "horizontal"
+          ? listElement.clientWidth
+          : listElement.clientHeight);
+      if (total > listElementHeight - rest.itemSize - 10) {
+        lastScrollOffset.current = scrollOffset;
+        loadMoreData();
+      }
+    }
+  };
+
+  return (
+    <>
+      <List
+        itemCount={result.length}
+        itemData={result}
+        outerRef={listRef}
+        onScroll={scrollHandler}
+        initialScrollOffset={lastScrollOffset.current}
+        {...rest}
+      >
+        {listItem}
+      </List>
+      {isLoading && loadingElement}
+      {isError && loadingElement}
+    </>
+  );
 };
 
 export default InfiniteLoader;
